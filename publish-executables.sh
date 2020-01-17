4c8094ef96904b5d61e4763c482bd8a0241d30ec
Sergio Rafael Gianazza <sgianazza@gmail.com>
2020-01-17 20:31:43 -0300

    upload tgz with executables

--- publish-executables.sh
+++ publish-executables.sh
@@ -14,9 +14,18 @@ pkg -t node10-linux-x64 package.json --output dist/$VERSION/linux/diffjam
 pkg -t node10-win-x64 package.json --output dist/$VERSION/windows/diffjam.exe
 pkg -t node10-macos-x64 package.json --output dist/$VERSION/osx/diffjam
 
+cd  dist/$VERSION/linux
+tar czvf diffjam-$VERSION.tgz ./diffjam
+cd ../osx
+tar czvf diffjam-$VERSION.tgz ./diffjam
+cd ../../..
+
+
 #PUBLISH TO S3
 aws s3 sync dist/ s3://diffjam --profile=diffjam
 
 aws s3api put-object-acl --bucket diffjam --key $VERSION/linux/diffjam --acl public-read --profile=diffjam
+aws s3api put-object-acl --bucket diffjam --key $VERSION/linux/diffjam-$VERSION.tgz --acl public-read --profile=diffjam
 aws s3api put-object-acl --bucket diffjam --key $VERSION/windows/diffjam.exe --acl public-read --profile=diffjam
 aws s3api put-object-acl --bucket diffjam --key $VERSION/osx/diffjam --acl public-read --profile=diffjam
+aws s3api put-object-acl --bucket diffjam --key $VERSION/osx/diffjam-$VERSION.tgz --acl public-read --profile=diffjam
