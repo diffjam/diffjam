8121b7036276331e5e2c2b63a65d0950eb8c9f51
Gregg Caines <gregg@caines.ca>
2020-07-28 17:41:31 +0000

    handle empty git grep results properly.  Version 2.0.1

--- countPolicy.js
+++ countPolicy.js
@@ -2,10 +2,13 @@ const pshell = require("pshell");
 
 module.exports = async (policy) => {
   let commandOutput;
+  const {command} = policy;
+  console.log("command: ", command)
   try {
-    commandOutput = await pshell(policy.command, {
+    commandOutput = await pshell(command, {
       echoCommand: false,
-      captureOutput: true
+      captureOutput: true,
+      ignoreError: true,  // `git grep` errors with code 1 if it finds nothing, so we need to ignoreErrors :\
     });
   } catch (ex) {
     console.error("error running shell command ", ex);
@@ -13,9 +16,17 @@ module.exports = async (policy) => {
     throw new Error("some error getting matches for countPolicy");
   }
   if (commandOutput.code !== 0) {
-    throw new Error("some error getting matches for countPolicy");
+    const errorIsOkay = commandOutput.code === 1 && !commandOutput.stderr && commandOutput.stdout === "";
+    // try to identify empty `git grep` results and not make them error. ^^^ 
+    if (!errorIsOkay) {
+      console.log("commandOutput: ", commandOutput);
+      console.error("policy: ", policy);
+      throw new Error("non-zero exit getting matches for countPolicy");
+    }
   }
+  console.log("commandOutput: ", commandOutput);
   examples = commandOutput.stdout.split("\n").filter(Boolean);
+  console.log("examples: ", examples);
   count = examples.length;
   return {count, examples};
 }
--- test/countPolicy.js
+++ test/countPolicy.js
@@ -1,6 +1,21 @@
 const countPolicy = require("../countPolicy");
 const expect = require("expect");
 
+const getException = async promise => {
+  try {
+    await promise;
+    throw new Error(`expected exception was not raised`);
+  } catch (ex) {
+    if (ex.message === "expected exception was not raised") {
+      console.log("throwing");
+      throw ex;
+    }
+    console.log(`returning: *${ex.message}*`)
+    return ex;
+  }
+};
+
+
 describe("#countPolicy", () => {
 
   it("counts lines and records examples", async () => {
@@ -11,5 +26,23 @@ describe("#countPolicy", () => {
     for (let i = 0; i < 10; i++) {
       expect(examples[i]).toBe(`line ${i}`);
     }
+  });
+
+  it("can handle when there are no lines at all", async () => {
+    // randomized output here so the command doesn't find this test file.
+    const command = `git grep ${new Date().getTime()}`;
+    const {count, examples} = await countPolicy({
+      command,
+    });
+    expect(examples).toHaveLength(0);
+    expect(count).toBe(0);
+  });
+
+  it("errors when something goes wrong", async () => {
+    const command = `asdf asdf`; // nonsensical command
+    const ex = await getException(countPolicy({
+      command,
+    }));
+    console.log("ex: ", ex);
   });
 })
