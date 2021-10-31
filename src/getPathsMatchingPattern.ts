e21048cf8c48dd77a7517d38602964c9c6e65144
Gregg Caines <gregg@caines.ca>
2021-10-31 00:57:05 +0000

    glob again from scratch. 3.0.2

--- src/getPathsMatchingPattern.ts
+++ src/getPathsMatchingPattern.ts
@@ -1,87 +1,154 @@
-const glob = require('glob-fs')({ dotfiles: false, gitignore:true })
+import { fdir } from "fdir";
+import findup from 'findup-sync';
+import ignore from 'parse-gitignore';
+import mm from 'micromatch';
+import fs from "fs";
 
-interface Pattern {
-    negated: boolean;
-    options: { withFileTypes: boolean };
-    original: string;
-    cwd: string;
-    isGlobstar: boolean;
-    parent: string;
-    base: string;
-    glob: string;
-    regex: RegExp;
+const cwd = process.cwd();
+
+const gitignoreFile = findup('.gitignore', {cwd: cwd});
+
+let ignorePatterns: string[] = [];
+if (gitignoreFile){
+  const fileContents = fs.readFileSync(gitignoreFile);
+  ignorePatterns = ignore(fileContents);
+}
+// console.log("ignorePatterns: ", ignorePatterns);
+
+
+const shouldIgnore = (fp: string) => {
+  // console.log("fp: ", fp);
+  
+  const retval = mm.any(fp, ignorePatterns);
+  // console.log("should ignore? ", retval);
+  
+  return retval;
 };
 
-interface Stats {
-    dev: number;
-    mode: number;
-    nlink: number;
-    uid: number;
-    gid: number;
-    rdev: number;
-    blksize: number;
-    ino: number;
-    size: number;
-    blocks: number;
-    atimeMs: number;
-    mtimeMs: number;
-    ctimeMs: number;
-    birthtimeMs: number;
-    atime: Date;
-    mtime: Date;
-    ctime: Date;
-    birthtime: Date;
-};
+// const glob = require('glob-fs')({ dotfiles: false, gitignore:true })
 
-interface GlobFile {
-    cache: unknown;
-    history: unknown[];
-    pattern: Pattern;
-    recurse: boolean;
-    dirname: string;
-    segment: string;
-    path: string;
-    orig: string;
-    stat: Stats;
-    isDirectory: () => boolean;
-    isFile: () => boolean;
-    isSymlink: () => boolean;
-    parse: () => string;
-    isDotfile: () => boolean;
-    isDotdir: () => boolean;
-    toAbsolute: () => string;
-    endsWith: (s: string) => boolean;
-    startsWith: (s: string) => boolean;
-    relative: string;
-    isAbsolute: true,
-    absolute: string;
-    root: string;
-    basename: string;
-    extname: string;
-    name: string;
-    exclude: boolean;
-  }
+// interface Pattern {
+//     negated: boolean;
+//     options: { withFileTypes: boolean };
+//     original: string;
+//     cwd: string;
+//     isGlobstar: boolean;
+//     parent: string;
+//     base: string;
+//     glob: string;
+//     regex: RegExp;
+// };
+
+// interface Stats {
+//     dev: number;
+//     mode: number;
+//     nlink: number;
+//     uid: number;
+//     gid: number;
+//     rdev: number;
+//     blksize: number;
+//     ino: number;
+//     size: number;
+//     blocks: number;
+//     atimeMs: number;
+//     mtimeMs: number;
+//     ctimeMs: number;
+//     birthtimeMs: number;
+//     atime: Date;
+//     mtime: Date;
+//     ctime: Date;
+//     birthtime: Date;
+// };
+
+// interface GlobFile {
+//     cache: unknown;
+//     history: unknown[];
+//     pattern: Pattern;
+//     recurse: boolean;
+//     dirname: string;
+//     segment: string;
+//     path: string;
+//     orig: string;
+//     stat: Stats;
+//     isDirectory: () => boolean;
+//     isFile: () => boolean;
+//     isSymlink: () => boolean;
+//     parse: () => string;
+//     isDotfile: () => boolean;
+//     isDotdir: () => boolean;
+//     toAbsolute: () => string;
+//     endsWith: (s: string) => boolean;
+//     startsWith: (s: string) => boolean;
+//     relative: string;
+//     isAbsolute: true,
+//     absolute: string;
+//     root: string;
+//     basename: string;
+//     extname: string;
+//     name: string;
+//     exclude: boolean;
+//   }
   
 
-const ignoreMore = (f: GlobFile) => {
-  if (f.isDotfile() || f.basename === "diffjam.yaml") {
-    f.exclude = true;
-  }
-//   console.log("f: ", f);
+// const ignoreMore = (f: GlobFile) => {
+//   if (f.isDotfile() || f.basename === "diffjam.yaml") {
+//     f.exclude = true;
+//   }
+// //   console.log("f: ", f);
   
-  return f;
-};
+//   return f;
+// };
 
 // TODO it would be cool if this streamed instead
 // FIXME
-export const getPathsMatchingPattern = async (patterns: string) => {
-    const files = await glob
-        .use(ignoreMore)
-        .readdirSync(patterns, { withFileTypes: true }) as string[];
-    // console.log("files: ", files);
-    //    throw new Error("files ^^^^^^^^") 
-    
-    const retval = files.filter((file) => {return file !== "diffjam.yaml";});
-    
-    return retval;
+export const getPathsMatchingPattern = async (dir: string, patterns: string) => {
+  const files = await new fdir()
+    .withBasePath()
+    .glob(patterns)
+    .filter((path: string, isDir: boolean) => {
+
+      // no directories (but recurse)
+      if (isDir) return false;
+
+      // no diffjam.yaml
+      if (path === "diffjam.yaml") return false;
+
+      // ignore based on .gitignore
+      // console.log("path: ", path);
+      
+      if (shouldIgnore("./" + path)) {
+        return false;
+      }
+
+      return true;
+    })
+    .exclude((dirName, fullPath) => {
+      // console.log("fullPath: ", fullPath);
+      // console.log("dirName: ", dirName);
+      // console.log("cwd: ", cwd);
+      const testPath = fullPath.slice(cwd.length + 1, fullPath.length);
+      // console.log("testPath: ", testPath);
+  
+      if (dirName.startsWith(".")) {
+        // console.log('excluding dotfile');
+        return true;
+      }
+      
+      if (shouldIgnore(testPath) || shouldIgnore(testPath + "/")) {
+        // console.log('excluding ignored file');
+        return true;
+      }
+      // console.log("not ignored: ", testPath);
+      
+      if (testPath === 'node_modules') {
+        throw new Error("stop");
+      }
+      
+
+      return false;
+    })
+    .crawl(dir)
+    .withPromise() as string[];
+
+    return files;
 }
--- src/match.ts
+++ src/match.ts
@@ -11,13 +11,17 @@ export interface Match {
 }
 type MatchDict = { [key: string]: Match[] };
 
-export const findMatches = async (filePattern: string, search: RegExp[]) => {
-  const filePaths = await getPathsMatchingPattern(filePattern);
+export const findMatches = async (filePattern: string, search: RegExp[], dir: string = process.cwd()) => {
+  // const dir = process.cwd();
+  const filePaths = await getPathsMatchingPattern(dir, filePattern);
   const results: MatchDict = {};
   for (const path of filePaths) {
     // TODO do these in parallel
     const contents = fs.readFileSync(path, { encoding: "utf8", flag: "r" });
-    results[path] = findInString(path, search, contents);
+    const found = findInString(path, search, contents);
+    if (found.length > 0){
+      results[path] = found;
+    }
   }
   return results;
 };
