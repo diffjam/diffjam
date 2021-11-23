fd64cdf60f8f6e8a12bcbb329c1ea7fb567a66c6
Urjit Singh Bhatia <urjitsb87@gmail.com>
2021-11-22 17:55:35 -0800

    add a way to refresh the config when file changes on disk

--- src/configFile.ts
+++ src/configFile.ts
@@ -2,23 +2,29 @@ import { exists as fileExists, readFileSync, writeFileSync } from "mz/fs";
 import { Config } from "./Config";
 import { Policy } from "./Policy";
 let config: Config;
-const defaultFilePath = './diffjam.yaml';
+let configLoaded = false;
+const defaultFilePath = "./diffjam.yaml";
 
-export async function exists(file = defaultFilePath){
+export async function exists(file = defaultFilePath) {
   return fileExists(file);
 }
 
-export async function getConfig(file = defaultFilePath): Promise<Config> {
-  if (config) {
-    return config;
-  }
+export async function refresh(file = defaultFilePath): Promise<Config> {
   const exists = await fileExists(file);
   if (exists) {
     config = Config.fromYaml(readFileSync(file).toString());
+    configLoaded = true;
   } else {
     config = new Config({});
   }
   return config;
+}
+
+export async function getConfig(file = defaultFilePath): Promise<Config> {
+  if (configLoaded) {
+    return config;
+  }
+  return refresh(file);
 }
 
 export const writeConfig = (conf: Config, filePath = defaultFilePath) => {
@@ -39,4 +45,8 @@ export function setPolicyBaseline(name: string, count: number) {
   const policy = config.getPolicy(name);
   policy.baseline = count;
   writeConfig(config);
+}
+
+export function clear() {
+  config = new Config({});
 }
