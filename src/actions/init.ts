1bcbb63cc7bc544e985d29d12aeebc16891f2cc8
Will Weiss <will.weiss1230@classdojo.com>
2022-10-25 17:54:33 -0500

    better policy

--- src/actions/init.ts
+++ src/actions/init.ts
@@ -3,9 +3,8 @@ import * as configFile from "../configFile";
 
 export const actionInit = async function (filePath?: string) {
   if (await configFile.exists(filePath)) {
-    console.error("A diffjam.yaml already exists.  Skipping initialization.");
-    process.exitCode = 1;
-    return;
+    console.error(`A ${filePath} already exists. Skipping initialization.`);
+    process.exit(1);
   }
 
   configFile.writeConfig(configFile.exampleConfig(), filePath);
